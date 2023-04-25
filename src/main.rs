736bde1ae697d55fc12bf76d19b01d2e7accff8e
retroheim <60027238+retroheim@users.noreply.github.com>
2023-04-25 05:31:37 -0700

    Simplify main file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::{fs};
+use std::fs;
 use serde::{Serialize, Deserialize};
-use serenity::http::Http;
-use serenity::model::id::ChannelId;
+use serenity::{http::Http, model::id::ChannelId};
 use tokio::time::{Duration, sleep};
-use tools::set_interval;
-use tools::image_tools::{ocr, screenshot};
+use tools::{set_interval, image_tools::{ocr, screenshot}};
 pub mod tools;
 
 #[derive(Serialize, Deserialize)]
@@ -26,6 +24,7 @@ struct Data {
     crop: Crop,
     check_image_timer: u64
 }
+
 #[derive(Serialize, Deserialize)]
 struct Config {
     data: Data
@@ -42,8 +41,9 @@ async fn main() {
 async fn startup() {
 
     // Reading Configs for Startup Configurations and Warnings
-    let loop_config: String = fs::read_to_string("config.json").expect("unable to read file");
-    let config: Config = serde_json::from_str(&loop_config).expect("Unable to parse json");
+    let config: Config = fs::read_to_string("config.json")
+        .and_then(|s| serde_json::from_str(&s))
+        .expect("Unable to read and parse config.json");
 
     // Checks to see if warnings are turned on
     if config.data.show_warnings {
@@ -61,8 +61,9 @@ async fn startup() {
     set_interval(|| async {
 
         // Reading the configs
-        let jsonfile = fs::read_to_string("config.json").expect("unable to read file");
-        let config: Config = serde_json::from_str(&jsonfile).expect("unable to parse json");
+        let config: Config = fs::read_to_string("config.json")
+        .and_then(|s| serde_json::from_str(&s))
+        .expect("Unable to read and parse config.json");
 
         // Screenshots Main Display and Crops image.
         screenshot(&config.data.crop.x, &config.data.crop.y, &config.data.crop.width, &config.data.crop.height);
@@ -72,26 +73,19 @@ async fn startup() {
         let mut count = 0usize;
 
         // Loops through all keywords to see if message contains one. Effectively a for loop.
-        loop {
-            if count < &config.data.keywords.len() -1 {
-                count += 1;
+        for keyword in &config.data.keywords {
+            if msg.contains(keyword) {
+                let bot = Http::new(&config.data.token);
+                let channel_id = ChannelId(config.data.channel);
 
-                // Check if Read Message has a Keyword from Config File.
-                if msg.contains(&config.data.keywords[count]) {
-                    
-                    // Setup Discord Bot
-                    let bot = Http::new(&config.data.token);
-                    let channel_id = ChannelId(config.data.channel);
+                let sentmessage = channel_id.send_message(&bot, |m| {
+                    m.content(format!("<@&{}>", &config.data.role))
+                     .embed(|e| e.title(&msg).description(&config.data.server))
+                     .add_file("images/roblox.png")
+                }).await;
 
-                    // Sends Message to configured Discord Channel whilst notifying the role configured
-                    let sentmessage = channel_id.send_message(&bot, |m| {
-                        m.content(format!("<@&{}>", &config.data.role)).embed(|e| e.title(&msg).description(&config.data.server)).add_file("images/roblox.png")
-                    }).await;
-                    sentmessage.unwrap();
-                    println!("{}", &msg);
-                    break;
-                }
-            } else {
+                sentmessage.unwrap();
+                println!("{}", &msg);
                 break;
             }
         }
